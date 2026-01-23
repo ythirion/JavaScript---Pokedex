34bb270c5a084161ae66be9a48f83c8323554fe9
CDasse <clemence.dasse@yahoo.fr>
2026-01-23 16:15:39 +0100

    Feat: add web-component

---
+++ my-first-pokedex-coda/src/pokemon-card.ts
@@ -0,0 +1,27 @@
+import {renderPokemon} from "./pokemon-show.ts";
+
+class MyPokemonCard extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const id = this.getAttribute('id');
+        const name = this.getAttribute('name');
+        const img = this.getAttribute('img');
+
+        this.innerHTML = `<div class="pokemon-card" data-id-pokemon="${id}">
+             <h3>#${id} ${name}</h3>
+             <img src=${img} alt="Image de ${name}" 
+             height="100" onerror="this.src='src/img/favicon.png'; this.onerror=null;"> 
+             </div>`
+
+        this.querySelector('[data-id-pokemon]')?.addEventListener(
+            'click', () => {
+                renderPokemon(this.getAttribute('data-id-pokemon')!);
+            }
+        );
+    }
+}
+
+window.customElements.define('pokemon-card', MyPokemonCard);
